b1d77340b289cb01f7ef51fa455e558a01aaff4a
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2022-10-12 21:16:40 +0300

    v 0.9.0

--- scripts.js
+++ scripts.js
@@ -3,96 +3,170 @@
 //----------------------------------------
 const my_main_dom = document.getElementById("my_main");
 const my_screen_dom = document.getElementById("my_screen");
+const my_buffer_dom = document.getElementById("bufferScreen");
 const clear_button = document.getElementById("ctrl_1");
 const del_button = document.getElementById("ctrl_2");
+my_buffer_dom.textContent = "...";
+my_screen_dom.textContent = 0;
+
+//----------------------------------------
+//    CALCULATOR
+//----------------------------------------
+let firstOperand = null;
+let secondOperand = null;
+let operation = "";
+let operationLoaded = false;
+
+function calculate(firstOperand, secondOperand, operation) {
+	console.log("*************Calculate enter*************");
+	console.log(`${firstOperand} ${operation} ${secondOperand} = `);
+	if (operation === null) return;
+	if (secondOperand === 0 && operation == "div") {
+		console.log("You can not divide by 0 dummy!");
+		clear_screen();
+		return null;
+	}
+	my_screen_dom.textContent = "";
+	switch (operation) {
+		case "add":
+			return Number(firstOperand) + Number(secondOperand);
+		case "sub":
+			return Number(firstOperand) - Number(secondOperand);
+		case "mul":
+			return Number(firstOperand) * Number(secondOperand);
+		case "div":
+			return Number(firstOperand) / Number(secondOperand);
+		default:
+			return null;
+	}
+}
+function selectOperation(pressedKey) {
+	console.log(`First Operand loaded: ${firstOperand}`);
+	switch (pressedKey) {
+		case "+":
+			console.log("Selected ADD");
+			return "add";
+		case "-":
+			console.log("Selected SUB");
+			return "sub";
+		case "*":
+			console.log("Selected MUL");
+			return "mul";
+		case "/":
+			console.log("Selected DIV");
+			return "div";
+	}
+}
+
 //----------------------------------------
 //    FUNCTIONS
 //----------------------------------------
 function clear_screen() {
-  my_screen_dom.innerHTML = "";
+	firstOperand = 0;
+	secondOperand = null;
+	operation = "";
+	operationLoaded = false;
+	my_buffer_dom.textContent = "...";
+	my_screen_dom.textContent = 0;
 }
 function pop_character() {
-  my_screen_dom.textContent = my_screen_dom.textContent.slice(0, -1);
+	my_screen_dom.textContent = my_screen_dom.textContent.slice(0, -1);
 }
-function parse_screen(to_parse) {
-  console.log(`parse_screen called: ${to_parse}`);
-  /*let buffer = toString(to_parse[0]);
-  let num1;
-  let num2;
-  for(let i=1; i<to_parse.length;i++){
-    if(/[0-9]/.test(to_parse[i])){
-		console.log(buffer);
-    	if(buffer=='-'){
-			buffer += num1;
-			buffer.toString();
-			console.log(buffer);
-		}
-    }
-  }*/
-}
+
 function log_button(e) {
-  //console.log(e.key)
-  if (e.key == "Backspace" || e.key == "Delete") {
-    pop_character();
-  }
-  if (e.key == "c" || e.key == "C") {
-    clear_screen();
-  }
-  if (e.key == "Enter") {
-    parse_screen(my_screen_dom.textContent);
-  }
-  if (/\d|\/|\*|\-|\+|\./gi.test(e.key)) {
-    my_screen_dom.textContent += e.key;
-  }
-  if (my_screen_dom.textContent.length >= 11) {
-    my_screen_dom.textContent = "***ERROR***";
-  }
+	if (my_screen_dom.textContent.length >= 11) {
+		my_screen_dom.textContent = "***ERROR***";
+	} else if (/\+|\-|\*|\//gi.test(e.key)) {
+		if (!operationLoaded) {
+			my_buffer_dom.textContent = my_screen_dom.textContent;
+			firstOperand = my_buffer_dom.textContent;
+			my_screen_dom.textContent = "";
+			secondOperand = null;
+			operation = selectOperation(e.key);
+			operationLoaded = true;
+		} else {
+			if (my_screen_dom.textContent == "") {
+				operation = selectOperation(e.key);
+				return;
+			}
+			secondOperand = my_screen_dom.textContent;
+			my_buffer_dom.textContent = calculate(
+				firstOperand,
+				secondOperand,
+				operation
+			);
+			operation = selectOperation(e.key);
+			firstOperand = my_buffer_dom.textContent;
+		}
+	} else if (e.key == "Backspace" || e.key == "Delete") {
+		pop_character();
+	} else if (e.key == "c" || e.key == "C") {
+		clear_screen();
+	} else if (e.key == "Enter") {
+		if (my_screen_dom.textContent == "") {
+			return null;
+		}
+		if (operationLoaded) {
+			secondOperand = my_screen_dom.textContent;
+			my_buffer_dom.textContent = calculate(
+				firstOperand,
+				secondOperand,
+				operation
+			);
+			firstOperand = my_buffer_dom.textContent;
+			secondOperand = null;
+		}
+	} else if (/\d|\./gi.test(e.key)) {
+		if (my_screen_dom.textContent[0] == "0") {
+			my_screen_dom.textContent = "";
+		}
+		my_screen_dom.textContent += e.key;
+	}
 }
-function create_clickable_menu(arr) {
-  console.log(arr);
-  for (let i = 0; i < arr.length; i++) {
-    let button_id = 0;
-    let logged_value = "";
-    button_id = `but-${i + 1}`;
-    //console.log(`Button_id = ${button_id}`);
-    logged_value = arr[i];
-    //console.log(`Logged_value = ${logged_value}`);
-    document.getElementById(button_id).addEventListener("click", () => {
-      if (button_id == "but-15") {
-        parse_screen(my_screen_dom.textContent);
-      } else {
-        my_screen_dom.textContent += logged_value;
-        if (my_screen_dom.textContent.length >= 11) {
-          my_screen_dom.textContent = "***ERROR***";
-        }
-      }
-    });
-    console.log(`Linked but-${i + 1} --> ${logged_value}`);
-  }
-}
+
 //----------------------------------------
 //    UI
 //----------------------------------------
+function create_clickable_menu(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let button_id = 0;
+		let logged_value = "";
+		button_id = `but-${i + 1}`;
+
+		logged_value = arr[i];
+
+		document.getElementById(button_id).addEventListener("click", () => {
+			if (button_id == "but-15") {
+				parse_screen(my_screen_dom.textContent);
+			} else {
+				my_screen_dom.textContent += logged_value;
+				if (my_screen_dom.textContent.length >= 11) {
+					my_screen_dom.textContent = "***ERROR***";
+				}
+			}
+		});
+	}
+}
 window.addEventListener("keydown", log_button);
 clear_button.addEventListener("click", clear_screen);
 del_button.addEventListener("click", pop_character);
 create_clickable_menu([
-  "7",
-  "8",
-  "9",
-  "/",
-  "4",
-  "5",
-  "6",
-  "*",
-  "1",
-  "2",
-  "3",
-  "-",
-  ".",
-  "0",
-  "=",
-  "+",
+	"7",
+	"8",
+	"9",
+	"/",
+	"4",
+	"5",
+	"6",
+	"*",
+	"1",
+	"2",
+	"3",
+	"-",
+	".",
+	"0",
+	"=",
+	"+",
 ]);
 
 //----------------------------------------
